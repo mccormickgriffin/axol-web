ca3ae7bfb4c870a5ca55c4cf7c80ef5cd05971c0
Griffin McCormick <mccormickgriffin@gmail.com>
2023-10-05 16:35:56 -0400

    added environment variables to project

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  redirectUrl: 'http://localhost:4200/callback',
+  clientId: '',
+  scopes:
+    'playlist-modify-public playlist-modify-private ' +
+    'playlist-read-private user-library-modify ' +
+    'playlist-read-collaborative user-library-read',
+};
