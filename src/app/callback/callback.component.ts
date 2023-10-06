550fdcd3c54ce52d7836562487aad166c13b8029
Griffin McCormick <mccormickgriffin@gmail.com>
2023-10-06 16:27:07 -0400

    get code from query params in callback component

--- src/app/callback/callback.component.ts
+++ src/app/callback/callback.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-callback',
   templateUrl: './callback.component.html',
-  styleUrls: ['./callback.component.scss']
+  styleUrls: ['./callback.component.scss'],
 })
 export class CallbackComponent {
+  constructor(
+    private route: ActivatedRoute,
+  ) {}
 
+  ngOnInit(): void {
+    const code: string = this.route.snapshot.queryParams['code']; 
+  }
 }
