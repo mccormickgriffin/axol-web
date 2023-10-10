d92d3f5c9924876ba5dad5f1df65169861d7b393
Griffin McCormick <mccormickgriffin@gmail.com>
2023-10-09 23:18:11 -0400

    added local storage service

---
+++ src/app/services/local-storage/local-storage.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+
+  constructor() { }
+
+  setItem(key: string, value: any): void {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  getItem(key: string): any {
+    try {
+      const item: string | null = localStorage.getItem(key);
+      return JSON.parse(item);
+    } catch (error) {
+      return null;
+    }
+  }
+
+  removeItem(key: string): void {
+    localStorage.removeItem(key);
+  }
+
+  clear(): void {
+    localStorage.clear();
+  }
+}
